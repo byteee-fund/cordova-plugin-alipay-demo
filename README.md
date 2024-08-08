1d88c70219c025f8bd908ea279308a3f2208128d
Jason.z <281674669@qq.com>
2024-08-08 23:23:40 +0800

    modify js~

---
+++ README.md
@@ -0,0 +1,24 @@
+## About
+
+This a cordova project demo for [cordova-plugin-alipay](https://byteee.fund/project/cordova-plugin-alipay)
+
+
+
+![screenshot](screenshot.jpeg)
+
+
+## 使用
+
+```shell
+cordova platform add android 
+cordova platform add ios
+cordova plugin add ../cordova-plugin-alipay
+
+cordova build android
+cordova build ios
+```
+
+
+
+
+
---
+++ www/js/index.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) [2024] [BSF]
+ * [https://byteee.fund]
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  console.log("device ready!");
+
+
+  window.Alipay.getVersion(
+    function (response) {
+      document.getElementById("versionResult").innerText = response;
+    },
+    function (error) {
+      alert(JSON.stringify(error));
+    }
+  );
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("loginButton").addEventListener("click", auth);
+    document.getElementById("payButton").addEventListener("click", pay);
+});
+
+
+function auth() {
+  window.Alipay.authV2(
+    { authInfo: 'apiname=com.alipay.account.auth&app_id=xxxxx&app_name=mc&auth_type=AUTHACCOUNT&biz_type=openservice&method=alipay.open.auth.sdk.code.get&pid=xxxxx&product_id=APP_FAST_LOGIN&scope=kuaijie&sign_type=RSA2&target_id=20141225xxxx&sign=fMcp4GtiM6rxSIeFnJCVePJKV43eXrUP86CQgiLhDHH2u%2FdN75eEvmywc2ulkm7qKRetkU9fbVZtJIqFdMJcJ9Yp%2BJI%2FF%2FpESafFR6rB2fRjiQQLGXvxmDGVMjPSxHxVtIqpZy5FDoKUSjQ2%2FILDKpu3%2F%2BtAtm2jRw1rUoMhgt0%3D' },
+    function (response) {
+      alert(JSON.stringify(response));
+    },
+    function (error) {
+      alert(JSON.stringify(error));
+    }
+  );
+}
+
+function pay() {
+    window.Alipay.payV2(
+        { orderInfo: "app_id=2015052600090779&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22seller_id%22%3A%22%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.02%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%22314VYGIAGG7ZOYY%22%7D&charset=utf-8&method=alipay.trade.app.pay&sign_type=RSA2&timestamp=2016-08-15%2012%3A12%3A15&version=1.0&sign=MsbylYkCzlfYLy9PeRwUUIg9nZPeN9SfXPNavUCroGKR5Kqvx0nEnd3eRmKxJuthNUx4ERCXe552EV9PfwexqW%2B1wbKOdYtDIb4%2B7PL3Pc94RZL0zKaWcaY3tSL89%2FuAVUsQuFqEJdhIukuKygrXucvejOUgTCfoUdwTi7z%2BZzQ%3D" },
+        function (response) {
+            alert(JSON.stringify(response));
+        },
+        function (error) {
+            alert(JSON.stringify(error));
+        }
+    );
+}
+
+
+
+
