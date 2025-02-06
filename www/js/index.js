63f80880379939e9afca9039e30576e7d47699f4
Jason.z <281674669@qq.com>
2025-02-06 10:38:45 +0800

    【修改】演示代码

--- www/js/index.js
+++ www/js/index.js
@@ -57,7 +57,7 @@ function auth() {
 
 function pay() {
     window.Alipay.payV2(
-        { orderInfo: "app_id=2015052600090779&biz_content=%7B%22timeout_express%22%3A%2230m%22%2C%22seller_id%22%3A%22%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22total_amount%22%3A%220.02%22%2C%22subject%22%3A%221%22%2C%22body%22%3A%22%E6%88%91%E6%98%AF%E6%B5%8B%E8%AF%95%E6%95%B0%E6%8D%AE%22%2C%22out_trade_no%22%3A%22314VYGIAGG7ZOYY%22%7D&charset=utf-8&method=alipay.trade.app.pay&sign_type=RSA2&timestamp=2016-08-15%2012%3A12%3A15&version=1.0&sign=MsbylYkCzlfYLy9PeRwUUIg9nZPeN9SfXPNavUCroGKR5Kqvx0nEnd3eRmKxJuthNUx4ERCXe552EV9PfwexqW%2B1wbKOdYtDIb4%2B7PL3Pc94RZL0zKaWcaY3tSL89%2FuAVUsQuFqEJdhIukuKygrXucvejOUgTCfoUdwTi7z%2BZzQ%3D" },
+        { orderInfo: "alipay_sdk=alipay-sdk-java-4.4.2.ALL&app_id=2021003110650758&biz_content=%7B%22body%22%3A%22%22%2C%22out_trade_no%22%3A%2210000240828090000010%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22%E8%A1%A3%E6%9C%B5%E4%BA%91%E9%AD%94%E6%96%B9-%E5%BA%94%E7%94%A8%E5%85%85%E5%80%BC%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.01%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=https%3A%2F%2Fpay-cube.test.ydoyun.com%2Fali%2Fnotify&sign=jP606BIM2FoCB12QUQ0JgejtfINdebGnOqmDP9T7YmEOAiN4%2B8UV7zK1gXip%2Fch31EYfJEcvRDB2FG%2BGNzDT2FGq368YZHW2VrLXhARuLyJ37bXtL%2BQMCZdDNzkEsTIZZyAR4upI9zUp%2BW2L72k1fiGaY5angMa%2FXlf1jldh8XbVoqRKUfxmIcs4Yb3r%2Bgl7VEuB%2FDrA5Rqy8c%2BM60XGMxbHbYUgcAIvL7azQSQBSrx8%2FcinmLVAe%2FYQ5D3uKjQO8nMOPRHih9RY6%2FLkF7jH4hBHZkU3ZLcxJBqB6UAccIPFrGiRoWtZqIcaoRmuFPJqGhpaW8T3llYtBtI94yiD5Q%3D%3D&sign_type=RSA2&timestamp=2024-08-28+09%3A46%3A10&version=1.0" },
         function (response) {
             alert(JSON.stringify(response));
         },
